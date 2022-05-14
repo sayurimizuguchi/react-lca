f1cf2b55d9c2401909ecb0b50a6f0661e4744b18
Sayuri Mizuguchi <sayuri@lazertechnologies.com>
2022-05-14 19:06:07 -0400

    few tweaks for deserialize; add LCA recursive

--- src/models/Node.ts
+++ src/models/Node.ts
@@ -1,28 +1,42 @@
 export const deserializeBT = (arr: (number | null)[]): Node | null => {
-  if (!arr.length || arr[0] == null) {
+
+  // remove second if cause what if first element is null and the rest are valid
+  //  || arr[0] == null */
+  if (!arr.length) {
     return null;
   }
 
-  const stack: Node[] = [];
   const root = new Node(arr.shift()!);
-  stack.push(root);
-  while (stack.length && arr.length) {
-    const node = stack.shift();
+  const queue: Node[] = [root];
+  while (arr.length) {
+    let node = queue.shift(), val: number | null | undefined;
     if (!node) {
       return root;
     }
 
-    const lVal = arr.shift();
-    const rVal = arr.shift();
-
-    node.left = lVal ? new Node(lVal) : null;
-    node.right = rVal ? new Node(rVal) : null;
-
-    node.left && stack.push(node.left);
-    node.right && stack.push(node.right);
+    //Set the left and right child values
+    node.left = (val = arr.shift()) || val === 0 ? new Node(val) : null;
+    node.right = (val = arr.shift()) || val === 0 ? new Node(val) : null;
+    
+    //Add the left and right child to the queue
+    node.left && queue.push(node.left);
+    node.right && queue.push(node.right);
   }
 
   return root;
+};
+
+
+/**
+ * @param {Node} root
+ * @param {Node} firstInput
+ * @param {Node} secondInput
+ */
+export const lowestCommonAncestor = (root: Node, firstInput: Node, secondInput: Node): Node => {
+  if (!root || !root.left || !root.right || root === firstInput || root === secondInput) return root;
+  var resL = lowestCommonAncestor(root.left, firstInput, secondInput);
+  var resR = lowestCommonAncestor(root.right, firstInput, secondInput);
+  return (resL && resR) ? root : (resL || resR);
 };
 
 export default class Node {
